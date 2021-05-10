af0b0f88f6426f68f0c55cd346e76cf833f585ec
Suraj Verma <surajverma@rentalcars.com>
2021-05-10 19:45:50 +0100

    add inquirer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log("app started");
+const inquirer = require("inquirer");
+
+const init = async () => {
+  const questions = [
+    {
+      type: "input",
+      message: "What is the length of your password?",
+      name: "length",
+    },
+    {
+      type: "confirm",
+      message: "Do you want to include lowercase characters?",
+      name: "isLowercase",
+    },
+    {
+      type: "confirm",
+      message: "Do you want to include uppercase characters?",
+      name: "isUppercase",
+    },
+    {
+      type: "confirm",
+      message: "Do you want to include number characters?",
+      name: "isNumber",
+    },
+    {
+      type: "confirm",
+      message: "Do you want to include special characters?",
+      name: "isSpecialCharacter",
+    },
+  ];
+
+  const answers = await inquirer.prompt(questions);
+
+  console.log(answers);
+};
+
+init();
